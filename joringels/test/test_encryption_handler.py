2cbae4e30f604a65bba07dc7a894b5cd0d435487
Lars Mielke <larsmielke2@gmail.com>
2022-07-11 10:42:54 +0200

    os.makedirs in test_encryption_handler to avoid path not found error

--- joringels/test/test_encryption_handler.py
+++ joringels/test/test_encryption_handler.py
@@ -33,7 +33,7 @@ class UnitTest(unittest.TestCase):
 
     def mk_test_data(cls, fileDir, fileName="test_secrets", *args, **kwargs):
         if not os.path.isdir(fileDir):
-            os.mkdir(fileDir)
+            os.makedirs(fileDir)
         testFileName = f"{sts.appParams.get('decPrefix')}{fileName}.yml"
         testData = {"Joringel": "Jorinde"}
         with open(os.path.join(fileDir, testFileName), "w") as f:
