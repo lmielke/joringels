2a5a12db9a8fdd70fdc1b2d984881432c3eedd02
Lars Mielke <larsmielke2@gmail.com>
2022-08-11 17:58:13 +0200

    auto push without message

--- prcs/jo.serve.sh
+++ prcs/jo.serve.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
-# jo.serve.sh
+
+# Regular Colors
+BLACK="\[\033[0;30m\]"        # Black
+RED="\[\033[0;31m\]"          # Red
+GREEN="\[\033[0;32m\]"        # Green
+YEL="\[\033[0;33m\]"       # Yellow
+BLUE="\[\033[0;34m\]"         # Blue
+PURPLE="\[\033[0;35m\]"       # Purple
+CYAN="\[\033[0;36m\]"         # Cyan
+WHITE="\[\033[0;37m\]"        # White
+NC='\033[0m' # No Color
+
 
 source /etc/environment
+mkdir /home/gitlab-runner/.local/share/virtualenvs/joringels-TEFpyPgb/lib/python3.9/site-packages/joringels/logs
 cd /home/$USERNAME/python_venvs/libraries/joringels
 pipenv run jo serve -n digiserver -rt
