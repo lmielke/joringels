5358d8d6e052cc25ecdc7387dc8791ac3097c72b
Lars Mielke <larsmielke2@gmail.com>
2022-12-06 15:58:14 +0100

    upload docker file method fuckup bugfix

--- joringels/src/connectors/docker.py
+++ joringels/src/connectors/docker.py
@@ -15,10 +15,10 @@ class LOC:
         print(f"\n\tcopying from: {sourcePath} to {targetPath}")
         shutil.copyfile(sourcePath, targetPath)
 
-    def mk_src_target(self, exportPath:str, targetPath: str, rmPath: str = None, *args, **kwargs):
+    def mk_src_target(self, exportPath:str, targetDir: str, rmPath: str = None, *args, **kwargs):
         sourcePath = exportPath.replace(os.sep, "/")
-        targetPath = os.path.join(targetPath, os.path.basename(sourcePath))
-        return sourcePath, exportPath
+        targetPath = os.path.join(targetDir, os.path.basename(sourcePath))
+        return sourcePath, targetPath
 
 
 def main(*args, **kwargs):
